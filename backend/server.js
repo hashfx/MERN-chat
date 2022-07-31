296dd761958a9269d1b6ab077e714187341f6d8f
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-07-31 13:27:24 +0000

    add dummy data for testing API

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');  // import express
 const dotenv = require('dotenv');
+const { chats } = require('./data/data');
 
 const app = express();  // instance of express
 dotenv.config();
@@ -7,11 +8,23 @@ dotenv.config();
 // get request to / route
 // app.get('route', callback, response)
 app.get('/', (req, res) => {
-    res.send('Hello World');  // send response
+    res.send('API running...');  // send response
+});
+
+
+// dummy API data for testing purpose
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const oneChat = chats.find((c) => c._id === req.params.id);  // search for _id in API data
+    res.send(oneChat);
 });
 
 
 
 // start express server on port 5000
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log("Server started on port ${PORT}"))
+const PORT = process.env.PORT || 5000  // if PORT is not available, use port 5000
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
