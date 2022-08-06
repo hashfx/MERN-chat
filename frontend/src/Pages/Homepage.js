ba3ae36e65805a212a38df0d5348c877a7be9b5b
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-08-06 14:26:28 +0000

    Login SignUp design Homepage fix App.CSS fix

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,5 +1,7 @@
 import { Container, Box, Text, Tab, Tabs, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
 import React from 'react';
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const Homepage = () => {
   return (
@@ -14,23 +16,23 @@ const Homepage = () => {
         borderRadius="lg"
         borderWidth="1px"
       >
-        <Text fontSize='4xl' fontFamily='Work sans' color="black">
+        <Text fontSize="4xl" fontFamily="Work sans" textAlign='center'>
           Mern Chat App
         </Text>
       </Box>
 
       <Box bg="white" w="100%" p={4} borderRadius="lg" color="black" borderWidth="1px">
-        <Tabs variant='soft-rounded'>
+        <Tabs isFitted variant='soft-rounded'>
           <TabList mb="1em">
-            <Tab width="50%"><Login/></Tab>
-            <Tab width="50%"><SignUp/></Tab>
+            <Tab>Login</Tab>
+            <Tab>SignUp</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
-              <p>one!</p>
+              <Login />
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              <Signup />
             </TabPanel>
           </TabPanels>
         </Tabs>
