db66567d99692dab1acce91b09bb618156852b93
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-08-06 13:07:02 +0000

    Login and SignUp UI design Chakra UI

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Container, Box, Text, Tab, Tabs, TabList, TabPanels, TabPanel } from '@chakra-ui/react';
+import React from 'react';
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW='xl' centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize='4xl' fontFamily='Work sans' color="black">
+          Mern Chat App
+        </Text>
+      </Box>
+
+      <Box bg="white" w="100%" p={4} borderRadius="lg" color="black" borderWidth="1px">
+        <Tabs variant='soft-rounded'>
+          <TabList mb="1em">
+            <Tab width="50%"><Login/></Tab>
+            <Tab width="50%"><SignUp/></Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+
+
+    </Container>
   )
 }
 
-export default Homepage
+export default Homepage;
