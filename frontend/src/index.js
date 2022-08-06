8961ae0e373b685f61c2556ba80c76b627fe0fe7
hashfx <algebra.soni@gmail.com>
2022-08-06 15:56:44 +0530

    committed to avoid merge conflicts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
