2450ae6711903526d54c0daec949af4661c0a627
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-08-20 16:38:46 +0000

    login successful designed todo: api/user/login not found

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,5 +1,8 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Show, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 
 const Login = () => {
@@ -8,22 +11,75 @@ const Login = () => {
     const [show, setShow] = useState(false)  // for Hide:Show password
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const history = useHistory();
 
     const handleClick = () => setShow(!show);
-    const submitHandler = () => { };
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
+        }
+
+        // console.log(email, password);
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+
+            const { data } = await axios.post(
+                "/api/user/login",
+                { email, password },
+                config
+            );
+
+            // console.log(JSON.stringify(data));
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            history.push("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+    };
 
     return (
         <VStack spacing='5px' color='black'>  { /* vertical stack */}
 
             <FormControl id='email' isRequired>
                 <FormLabel>Email</FormLabel>
-                <Input placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+                <Input placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
             </FormControl>
 
             <FormControl id='password' isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                    <Input type={show ? "text" : 'password'} placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+                    <Input type={show ? "text" : 'password'} placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
                     <InputRightElement width='4.5em'>
                         <Button h="1.75rem" size="sm" onClick={handleClick}>
