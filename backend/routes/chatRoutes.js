48b332d2fbacb6f652083dff354d99db0fac73eb
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2022-08-24 16:05:45 +0000

    chatRoutes designed

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,1 +1,13 @@
-// 12:29
+const express = require("express")
+
+const router = express.Router()
+
+// only logged in users can access this route
+router.route('/').post(protect, accessChat)
+router.route('/').get(protect, fetchChats)  // fetch chat for particular user from database
+router.route('/group').post(protect, createGroupChat)  // route for creation of group
+router.route('/rename').put(protect, renameGroup)  // route for renaming the group
+router.route('/groupremove').put(protect, removeFromGroup)  // route for remove someone or leave the group
+router.route('/groupadd').put(protect, addToGroup)  // route for adding someone to the group
+
+module.exports = router;
